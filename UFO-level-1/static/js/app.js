5eca32191e0eddb80f66181456110675a5dc1fc1
Mitsuko Kitazawa <mitsuko.kitazawa@gmail.com>
2019-09-15 16:26:38 -0500

    complete level 1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,15 +1,7 @@
-// from data.js
-var tableData = data;
-
-// YOUR CODE HERE!
-console.log(tableData[0].datetime);
-
-var tableBody = d3.select("tbody")
-
-
+// Define functions 
 
 function build_table(varList)
-{
+{// Build table to show (i.e load data)
     tableBody.html("");
     
     varList.forEach(element => {
@@ -26,16 +18,38 @@ function build_table(varList)
 
 }
 
-build_table(tableData);
-
 function HandleClick()
-{
+{ // Define the filter actions
     var date = d3.select("#datetime").property("value");
     var filteredData = tableData; 
 
     filteredData = filteredData.filter(row => row.datetime === date)
-    build_table(filteredData);
+    
+    // Check if the filtered table has data
+    // If no data, send a warning and reset the page
+    if (filteredData.length == 0 ){
+      alert("Date not found!\nEnter a new date between 1/1/2010 and 1/13/2010");
+      document.getElementById("datetime").value="1/1/2010";
+      build_table(tableData);
+    }
+    // else show the filtered data
+    else {
+      build_table(filteredData);
+    }
+    
 
 }
 
+
+////////////////////
+// Main
+// from data.js
+// Assign table data in tableData
+var tableData = data;
+
+// Select table area using d3
+var tableBody = d3.select("tbody")
+
+
+build_table(tableData);
 d3.selectAll("#filter-btn").on("click", HandleClick);
